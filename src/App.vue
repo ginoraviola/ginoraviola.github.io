2507a13577f76ba8bf3fc5aa41def12b3ac2de29
Gino Raviola <ginoraviola@gmail.com>
2020-01-26 15:25:57 -0300

    renew

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Code&display=swap" rel="stylesheet">
+    <header-component></header-component>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+
+@Component({
+  name: 'App',
+  components: {
+    HeaderComponent,
+    HelloWorld,
+  },
+})
+export default class App extends Vue {
+}
+</script>
+
+<style>
+  @import "styles/main.css";
+  * {
+    margin: 0;
+    padding: 0;
+    background-color: #151515;
+  }
+#app {
+  font-family: 'Fira Code', monospaced;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #151515;
+  margin: 5px;
+  width: 100%;
+  height: 100%;
+}
+
+
+</style>
---
+++ src/resume-data.ts
@@ -0,0 +1,11 @@
+export default {
+  profile: {
+    name: 'Gino',
+    last_name: 'Raviola',
+    tagline: 'Front-end dev',
+    birth_date: '15-05-1996',
+    about: 'I was born in San Francisco, Cordoba - Argentina. Currently studying Systems Engineering and building my career as a software developer. Im 23 years old and very excited to learn as much as I can about Frontend Development. My main interests are software, music and amateur photography.',
+  },
+  sections: {
+  },
+};
