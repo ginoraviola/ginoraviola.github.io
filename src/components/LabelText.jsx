ff790709e0568bbd3569d363992a1007c1f69d8e
Gino Raviola <ginoraviola@gmail.com>
2022-08-18 17:19:46 -0300

    Updates

--- src/components/LabelText.jsx
+++
@@ -1,9 +0,0 @@
-const LabelText = ({ text }) => (
-  <div className="bg-black p-2 rounded-lg shadow-2xl">
-    <p className="text-white font-bold text-xs line-clamp-1">
-      {text}
-    </p>
-  </div>
-);
-
-export default LabelText;
